b1a2ca42e06ab46fda587942ac53338a71247235
Patrick Etienne <patricketienne@Patricks-MBP.attlocal.net>
2023-09-21 19:41:17 -0400

    adding missing functions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,21 @@
 const router = require("express").Router()
 const fs =require("fs")
-router.get("/notes", (req, res)=> {
+router.delete("/api/notes/:itemid",(req,res) => {
+
+})
+router.post("/", (req, res) => {
+   const readFile = fs.readFileSync("db/db.json").toString()
+   let json = JSON.parse(readFile)
+   console.log(req.body)
+   console.log(json)
+   json.push(req.body)
+   
+   fs.writeFileSync("db/db.json", JSON.stringify(json))
+    //Read file & use request.body to add info to my data
+    // after use fs.write file to save new data
+}
+) 
+router.get("/", (req, res)=> {
 
 fs.readFile("db/db.json","utf8", (err, data)=> {
     if (err) throw err
@@ -13,6 +28,8 @@ fs.readFile("db/db.json","utf8", (err, data)=> {
     } 
     return res.json(parseData)
 })
+
+
 
 })
 
--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use("/",htmlRoutes)
-app.use("/api",apiRoutes)
+app.use("/api/notes",apiRoutes)
 app.listen( PORT, ()=> console.log("server running"))
 
